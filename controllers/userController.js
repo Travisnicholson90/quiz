7cdf7c49a7e4dc354e6b1725662bfa559d4f8b16
Travis Nicholson <110001213+Travisnicholson90@users.noreply.github.com>
2023-06-29 10:28:48 +1000

    update user routes to get users quiz

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,4 +16,19 @@ const getUserById = async (req, res) => {
     }
 };
 
+const getUserQuizzes = async (req, res) => {
+    try {
+        const getUserQuiz = await User.findById(req.params.id).populate('quizzes.quiz');
+
+        if(!getUserQuiz) {
+            res.status(400).json({ message: 'User not found' });
+            return;
+        }
+
+        res.status(200).json(getUserQuiz);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
 module.exports = { getUserById };
